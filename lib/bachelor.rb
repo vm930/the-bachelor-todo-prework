26e9130ab0862530b0517ffeea6c307045888181
abstract-animation-9914 <vm930@hotmail.com>
2019-02-11 19:32:36 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -28,20 +28,6 @@ end
 
 
 
-
-
-
-
-# def get_contestant_name(data, occupation)
-#   data.each do |season, contestants|
-#     contestants.each do |contestant_hash|
-#       if contestant_hash["occupation"] == occupation
-#         return contestant_hash["name"]
-#       end
-#     end
-#   end
-# end
-
 # def count_contestants_by_hometown(data, hometown)
 #   count = 0
 #   data.each do |season, contestants|
