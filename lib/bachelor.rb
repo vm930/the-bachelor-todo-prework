fedced53406cb7d9e961dff6b0b36bc43426ad9d
abstract-animation-9914 <vm930@hotmail.com>
2019-02-11 19:54:08 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -48,7 +48,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |season_hash|
     average_age << season_hash["age"].to_i
   end
-  return Math.round average_age
+    average_age.inject do |sum, element| sum + element
 end 
 
 
