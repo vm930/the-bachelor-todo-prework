3002053e5f0f6ff685d311d121c884d3eb284bd3
abstract-animation-9914 <vm930@hotmail.com>
2019-02-11 19:43:05 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,25 +52,6 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-# def get_occupation(data, hometown)
-#   data.each do |season, contestants|
-#     contestants.each do |contestant_hash|
-#       if contestant_hash["hometown"] == hometown
-#         return contestant_hash['occupation']
-#       end
-#     end
-#   end
-# end
-
 # def get_average_age_for_season(data, season)
 #   age_total = 0
 #   num_of_contestants = 0
